e0b7dd75e676fc8f38bb8b7870d0ab45d5b5cce2
Tu Anh Dinh <dinhtuanh23@gmail.com>
2025-07-17 19:23:29 +0200

    Mask out input prompt during training

--- prepare_data.py
+++ prepare_data.py
@@ -1,6 +1,7 @@
 from datasets import Dataset
 from transformers import AutoTokenizer
 import torch
+import os
 
 def line_pairs(src_file, tgt_file):
     with open(src_file, "r", encoding="utf-8") as f_src, open(tgt_file, "r", encoding="utf-8") as f_tgt:
@@ -8,60 +9,38 @@ def line_pairs(src_file, tgt_file):
             yield {"input": src.strip(), "target": tgt.strip()}
 
 # Format: instruction + response
-def format_example(example, tokenizer, model_id):
-    if model_id == "Unbabel/TowerInstruct-7B-v0.2":
-        messages = [
-            {"role": "user", "content": f"Translate the following text from English into German.\nEnglish: {example['input']}.\nGerman:"},
-        ]
-        prompt = tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
-        prompt = prompt + example['target']
-    else:
-        raise NotImplementedError
+def format_and_tokenize_example(example, tokenizer, max_length=1024):
+    full_messages = [
+        {"role": "user", "content": f"Translate the following text from English into German.\nEnglish: {example['input']}.\nGerman:"},
+        {"role": "assistant", "content": example['target']}
+    ]
+    input_message = [full_messages[0]]
+    tokenized_full_prompt = tokenizer.apply_chat_template(full_messages, tokenize=True, add_generation_prompt=True, max_length=max_length, padding="max_length", truncation=True, return_dict=True)
+    input_prompt_length = len(tokenizer.apply_chat_template(input_message, tokenize=True, add_generation_prompt=True))
 
-    return {"text": prompt}
+    tokenized_full_prompt["labels"] = tokenized_full_prompt["input_ids"].copy()
 
-# Tokenize with labels (shifted targets)
-def tokenize_fn(example, tokenizer, max_length=1024):
-    # We treat the full prompt+response as a causal LM sequence
-    tokenized = tokenizer(
-        example["text"],
-        truncation=True,
-        max_length=max_length,
-        padding="max_length",
-    )
-    tokenized["labels"] = tokenized["input_ids"].copy()
-    return tokenized
+    # Mask out input prompt in the labels so we dont train on it later
+    tokenized_full_prompt["labels"][:input_prompt_length] = [tokenizer.pad_token_id] * input_prompt_length
+
+    return tokenized_full_prompt
+
 
 # Main function to build datasets
-def build_datasets(tokenizer, model_id, max_length=2048):
-    data_dir = "/hkfs/home/project/hk-project-p0021679/ho6084/fairseq/examples/confidence_aware_ssl/data/ParaCrawl"
+def build_datasets(tokenizer, max_length=1024):
+    data_dir = f"{os.environ.get('ROOT_DIR')}/fairseq/examples/confidence_aware_ssl/data/ParaCrawl"
     # Wrap with Hugging Face datasets
     train_dataset = Dataset.from_generator(lambda: line_pairs(f"{data_dir}/train.5M.dedup.en", f"{data_dir}/train.5M.dedup.de"))
     eval_dataset  = Dataset.from_generator(lambda: line_pairs(f"{data_dir}/dev.en", f"{data_dir}/dev.de"))
 
-    # Format with prompt
     train_dataset = train_dataset.map(
-        lambda x: format_example(x, tokenizer, model_id),
+        lambda x: format_and_tokenize_example(x, tokenizer, max_length),
         load_from_cache_file=True,
         num_proc=100
     )
     eval_dataset = eval_dataset.map(
-        lambda x: format_example(x, tokenizer, model_id),
+        lambda x: format_and_tokenize_example(x, tokenizer, max_length),
         load_from_cache_file=True,
-        num_proc=100
-    )
-
-    # Tokenize
-    train_dataset = train_dataset.map(
-        lambda ex: tokenize_fn(ex, tokenizer, max_length),
-        load_from_cache_file=True,
-        remove_columns=train_dataset.column_names,
-        num_proc=100
-    )
-    eval_dataset = eval_dataset.map(
-        lambda ex: tokenize_fn(ex, tokenizer, max_length),
-        load_from_cache_file=True,
-        remove_columns=eval_dataset.column_names,
         num_proc=100
     )
 
